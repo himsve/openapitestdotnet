08efc6ac586293c71a0b176d1f14079e1fcd6598
Sveinung Himle <himsve@kartverket.no>
2021-12-21 15:19:09 +0100

    Add new files

---
+++ OpenApi/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using OpenApi;
+
+namespace OpenApi
+{
+    class Program
+    {
+        public static bool WriteToConsole = true;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Transformation is started");
+
+            Transform(4258, 4312, 10d, 60d);
+            Transform(4258, 25832, 10d, 60d);            
+
+            TestOpenApiV1();
+            
+            Console.WriteLine("Transformation completed!");
+        }
+
+        static void Transform(int epsgSource, int epsgTarget, double x, double y, double z = 0d, double t = 0d)
+        {
+            try
+            {
+                using var httpClient = new System.Net.Http.HttpClient();
+
+                var oa = new OpenApiV1.TransformerClient(httpClient);
+
+                OpenApiV1.Epsg epsgS = (OpenApiV1.Epsg)Enum.ToObject(typeof(OpenApiV1.Epsg), epsgSource);
+                OpenApiV1.Epsg epsgT = (OpenApiV1.Epsg)Enum.ToObject(typeof(OpenApiV1.Epsg), epsgTarget);
+
+                var res = oa.GetAsync(x, y, z, t, epsgS, epsgT).Result;
+            }
+            catch (AggregateException ex)
+            {
+            }
+            catch (Exception ex)
+            {   
+            }
+        }
+
+        static void TestOpenApiV1()
+        {
+            using var httpClient = new System.Net.Http.HttpClient();
+
+            var oa = new OpenApiV1.TransformerClient(httpClient);
+
+            var x = 9d; var y = 60d;
+            var rand = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var xrand = x + rand.NextDouble();
+                var yrand = y + rand.NextDouble();
+                var z = 200d + rand.NextDouble()*50d;
+                var t = 2020d;
+
+                //var res = oa.GetAsync(xrand, yrand, z, t, OpenApiV1.Epsg._4258, OpenApiV1.Epsg._25832).Result;
+                var res = oa.GetAsync(xrand, yrand, z, t, OpenApiV1.Epsg._7912, OpenApiV1.Epsg._4937).Result;                
+
+                if (WriteToConsole)
+                    Console.WriteLine($"Input: x {xrand} y {yrand} z {z} t {t}, Output: x {res.X} y {res.Y} z {res.Z}");
+            }            
+        }        
+    }
+}
---
+++ openapinunit/OpenApiTest.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using OpenApi;
+
+namespace openapinunit
+{
+    public class OpenApiTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
